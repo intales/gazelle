d229341b64817ba7325650b1390b4a548d521f90
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2024-03-21 17:18:06 +0100

    library docs

--- lib/gazelle.dart
+++ lib/gazelle.dart
@@ -1,3 +1,14 @@
+/// Welcome to Gazelle Framework – your gateway to crafting high-performance web applications in Dart!
+///
+/// With Gazelle, you'll harness the power of efficient route matching using Trie data structures,
+/// seamlessly handle HTTP requests and responses, and effortlessly build custom routers and middleware.
+///
+/// Gazelle embraces a modular architecture, allowing you to extend its functionality through custom plugins.
+/// Whether you're integrating authentication, logging, or other common features, Gazelle's plugin system
+/// empowers you to tailor your application to your specific needs.
+///
+/// Empower your web development journey with Gazelle Framework and unlock the potential to create
+/// blazing-fast and scalable server-side applications with ease. Let's embark on this exciting adventure together!
 library;
 
 export 'src/gazelle_base.dart';
