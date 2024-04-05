a6a83e0cc26e0fa895c396818835fb0375751af9
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2024-04-05 15:29:23 +0200

    Benchmarks (#5)
    * Initial benchmarks
    
    Added basic benchmark project for Gazelle
    
    * Benchmark test fix
    
    * Benchmark docs

---
+++ packages/gazelle_benchmark/test/gazelle_benchmark_test.dart
@@ -0,0 +1,16 @@
+import 'package:gazelle_benchmark/gazelle_benchmark.dart';
+import 'package:http/http.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Should start and stop benchmark server', () async {
+    await startGazelleServer();
+
+    final response = await get(Uri.parse("http://localhost:3000/"));
+
+    expect(response.statusCode, 200);
+    expect(response.body, "{ hello: 'world' }");
+
+    await stopGazelleServer();
+  });
+}
