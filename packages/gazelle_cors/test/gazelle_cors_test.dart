1ad34c554e46767d92e4e5ea212e81bb37dcd31f
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2024-04-10 19:47:34 +0200

    Added pre request hook

--- packages/gazelle_cors/test/gazelle_cors_test.dart
+++ packages/gazelle_cors/test/gazelle_cors_test.dart
@@ -1,7 +1,37 @@
-import 'package:test/scaffolding.dart';
+import 'package:gazelle_core/gazelle_core.dart';
+import 'package:gazelle_cors/src/gazelle_cors_plugin.dart';
+import 'package:http/http.dart' as http;
+import 'package:test/test.dart';
 
 void main() {
   group('GazelleCorsPlugin tests', () {
-    test('Dummy test', () {});
+    test('Should add CORS headers', () async {
+      // Arrange
+      final app = GazelleApp();
+      await app.registerPlugin(GazelleCorsPlugin());
+      app.get(
+        "/",
+        (request) async {
+          return GazelleResponse(
+            statusCode: 200,
+            headers: request.headers,
+            body: "Hello, Gazelle!",
+          );
+        },
+        preRequestHooks: [app.getPlugin<GazelleCorsPlugin>().corsHook],
+      );
+
+      await app.start();
+
+      // Act
+      final url = Uri.parse("http://${app.address}:${app.port}/");
+      final result = await http.get(url, headers: {'origin': 'example.com'});
+
+      // Assert
+      print(result.headers);
+      expect(result.statusCode, 200);
+
+      await app.stop();
+    });
   });
 }
