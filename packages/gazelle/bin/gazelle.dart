49e650fe2f3b9426203664a007b7697f673caff8
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2024-04-16 11:50:48 +0200

    Added gazelle cli package

---
+++ packages/gazelle/bin/gazelle.dart
@@ -0,0 +1,5 @@
+import 'package:gazelle/gazelle.dart' as gazelle;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gazelle.calculate()}!');
+}
---
+++ packages/gazelle/test/gazelle_test.dart
@@ -0,0 +1,8 @@
+import 'package:gazelle/gazelle.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
