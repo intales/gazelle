b5af50383fe2d054f4c19447bddce08a6c5a4f8c
Erfan Soltanzadeh <soltanzadeh836@gmail.com>
2024-06-12 18:37:52 +0330

    Update gazelle_core_example.dart
    Error Handling: Wrapped the server start logic in a try-catch block to handle any exceptions that might occur during startup.
    
    Future Initialization: Changed the main function to explicitly return a Future<void> to emphasize its asynchronous nature.
    
    Code Formatting: Improved formatting for better readability.

--- packages/gazelle_core/example/gazelle_core_example.dart
+++ packages/gazelle_core/example/gazelle_core_example.dart
@@ -1,18 +1,22 @@
 import 'package:gazelle_core/gazelle_core.dart';
 
-void main() async {
-  final app = GazelleApp(
-    routes: [
-      GazelleRoute(
-        name: "hello_gazelle",
-        get: (context, request, response) => GazelleResponse(
-          statusCode: GazelleHttpStatusCode.success.ok_200,
-          body: "Hello, Gazelle!",
+Future<void> main() async {
+  try {
+    final app = GazelleApp(
+      routes: [
+        GazelleRoute(
+          name: "hello_gazelle",
+          get: (context, request, response) => GazelleResponse(
+            statusCode: GazelleHttpStatusCode.success.ok_200,
+            body: "Hello, Gazelle!",
+          ),
         ),
-      ),
-    ],
-  );
+      ],
+    );
 
-  await app.start();
-  print("Gazelle listening at ${app.serverAddress}");
+    await app.start();
+    print("Gazelle listening at ${app.serverAddress}");
+  } catch (e) {
+    print("Failed to start the server: $e");
+  }
 }
