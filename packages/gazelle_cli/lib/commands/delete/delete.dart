f7763a1da2b3a2467cd3813b09d7a0f5ffa98587
raman04-byte <ramantank04022002@gmail.com>
2024-06-22 14:25:19 +0530

    fix: project configuration error solved

--- packages/gazelle_cli/lib/commands/delete/delete.dart
+++ packages/gazelle_cli/lib/commands/delete/delete.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:args/command_runner.dart';
 import 'package:cli_spin/cli_spin.dart';
+import '../../commons/entities/project_configuration.dart';
 import '../../commons/functions/confirmation.dart';
 import '../../commons/functions/load_project_configuration.dart';
 import 'delete_project.dart';
@@ -25,15 +26,25 @@ class DeleteCommand extends Command {
   @override
   void run() async {
     final pathOption = argResults?.option("path");
-    final projectConfiguration =
-        await loadProjectConfiguration(path: pathOption);
-    final answer = getConfirmation(
-        '\nAre you sure you want to delete the project ${projectConfiguration.name}');
-
     final spinner = CliSpin(
       text: "Deleting Gazelle project...",
       spinner: CliSpinners.dots,
     ).start();
+    late final ProjectConfiguration projectConfiguration;
+    try {
+      projectConfiguration = await loadProjectConfiguration(path: pathOption);
+    } on LoadProjectConfigurationGazelleNotFoundError catch (e) {
+      spinner.fail(e.errorMessage);
+      exit(2);
+    } on LoadProjectConfigurationPubspecNotFoundError catch (e) {
+      spinner.fail(e.errorMessage);
+      exit(2);
+    } on Exception catch (e) {
+      spinner.fail(e.toString());
+      exit(2);
+    }
+    final answer = getConfirmation(
+        '\nAre you sure you want to delete the project ${projectConfiguration.name}');
     if (!answer) {
       spinner.fail("Project deletion aborted.");
       return;
@@ -45,9 +56,6 @@ class DeleteCommand extends Command {
       );
     } on DeletingProjectError catch (e) {
       spinner.fail(e.message);
-      exit(2);
-    } on LoadProjectConfigurationGazelleNotFoundError catch (e) {
-      spinner.fail(e.errorMessage);
       exit(2);
     } on Exception catch (e) {
       spinner.fail(e.toString());
