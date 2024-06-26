ffdcef006fee0f37dd79cd2175cfa35f62b4fb37
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2024-06-26 15:37:14 +0200

    fix(gazelle_cli): Removed "prova"
    Removed "prova" test project.

--- packages/gazelle_cli/prova/bin/prova.dart
+++
@@ -1,5 +0,0 @@
-import "package:prova/prova.dart" as prova;
-
-Future<void> main(List<String> args) async {
-  prova.runApp(args);
-}
--- packages/gazelle_cli/prova/lib/routes/hello_gazelle_route/hello_gazelle_route.dart
+++
@@ -1,7 +0,0 @@
-import 'package:gazelle_core/gazelle_core.dart';
-import 'handlers/hello_gazelle_get_handler.dart';
-
-const helloGazelleRoute = GazelleRoute(
-  name: "hello_gazelle",
-  get: helloGazelleGet,
-);
