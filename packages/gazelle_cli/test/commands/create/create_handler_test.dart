68b62cf283c635e2f5ff2df5ca8d8e8d776b7db1
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-07-03 18:11:50 +0530

    feat(gazelle_cli): Added an exhaustive tests for create handler command
    Added exhaustive tests for the `createHandler` command in the `gazelle_cli` package. The tests ensure that the handler files are created with the correct content based on the provided route name and HTTP method. This improves the reliability and correctness of the `createHandler` command.

--- packages/gazelle_cli/test/commands/create/create_handler_test.dart
+++ packages/gazelle_cli/test/commands/create/create_handler_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:dart_style/dart_style.dart';
 import 'package:gazelle_cli/commands/create/create_handler.dart';
 import 'package:test/test.dart';
 
@@ -25,6 +26,63 @@ void main() {
       expect(result.handlerName, "helloWorldGet");
       expect(File(result.handlerFilePath).existsSync(), isTrue);
 
+      // Clean up
+      await tmpDir.delete(recursive: true);
+    });
+    test('Should create handler files with the correct content', () async {
+      // Arrange
+      final tmpDirPath = "tmp/create_handler_exhaustive_tests";
+      final routeName = "hello_world";
+
+      Directory tmpDir = Directory(tmpDirPath);
+      if (await tmpDir.exists()) {
+        await tmpDir.delete(recursive: true);
+      }
+      tmpDir = await Directory(tmpDirPath).create(recursive: true);
+
+      String getHandlerContent(String handlerName) {
+        return DartFormatter().format("""
+import 'package:gazelle_core/gazelle_core.dart';
+
+GazelleResponse $handlerName(
+  GazelleContext context,
+  GazelleRequest request,
+  GazelleResponse response,
+) {
+  return GazelleResponse(
+    statusCode: GazelleHttpStatusCode.success.ok_200,
+    body: "Hello, World!",
+  );
+}
+  """
+            .trim());
+      }
+
+      for (final httpMethod in ["GET", "POST", "PUT", "PATCH", "DELETE"]) {
+        String handlerName = switch (httpMethod) {
+          "GET" => "Get",
+          "POST" => "Post",
+          "PUT" => "Put",
+          "PATCH" => "Patch",
+          "DELETE" => "Delete",
+          _ => throw "Unexpected error",
+        };
+        final expectedHandlerName = "helloWorld$handlerName";
+
+        final handlerContent = getHandlerContent(expectedHandlerName);
+
+        // Act
+        final result = await createHandler(
+          routeName: routeName,
+          httpMethod: httpMethod,
+          path: tmpDirPath,
+        );
+
+        // Assert
+        expect(result.handlerName, expectedHandlerName);
+        expect(File(result.handlerFilePath).existsSync(), isTrue);
+        expect(File(result.handlerFilePath).readAsStringSync(), handlerContent);
+      }
       // Clean up
       await tmpDir.delete(recursive: true);
     });
