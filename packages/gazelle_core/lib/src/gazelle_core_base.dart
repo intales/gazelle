765f33b5c3044c5fbffe70a595a2270b77d6c3a1
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2024-07-13 14:56:05 +0200

    refactor(gazelle_core): serialization
    Replaced serialization logic with
    gazelle_serialization package.

--- packages/gazelle_core/lib/src/gazelle_core_base.dart
+++ packages/gazelle_core/lib/src/gazelle_core_base.dart
@@ -1,3 +1,5 @@
+export 'package:gazelle_serialization/gazelle_serialization.dart';
+
 export 'gazelle_app.dart';
 export 'gazelle_context.dart';
 export 'gazelle_hooks.dart';
@@ -5,8 +7,6 @@ export 'gazelle_http_header.dart';
 export 'gazelle_http_method.dart';
 export 'gazelle_http_status_code.dart';
 export 'gazelle_message.dart';
-export 'gazelle_model_provider.dart';
-export 'gazelle_model_type.dart';
 export 'gazelle_plugin.dart';
 export 'gazelle_route.dart';
 export 'gazelle_router.dart';
