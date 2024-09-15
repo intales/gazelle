0401d7da69cde993c46b94f854b9126edecdec6d
Raman Tank <ramantank04022002@gmail.com>
2024-09-15 15:56:38 +0530

    Refactor delete command and error handling
    - Renamed DeletingProjectError to DeleteProjectError for consistency.
    
    Closes #<issue_number>

--- packages/gazelle_cli/lib/commands/delete/delete_project.dart
+++ packages/gazelle_cli/lib/commands/delete/delete_project.dart
@@ -1,22 +1,22 @@
 import 'dart:io';
 
 /// Uninstalls a Gazelle project.
-class DeletingProjectError {
+class DeleteProjectError {
   /// The error message.
   final String message;
 
-  /// Creates a [DeletingProjectError].
-  const DeletingProjectError() : message = "No Gazelle project found.";
+  /// Creates a [DeleteProjectError].
+  const DeleteProjectError() : message = "No Gazelle project found.";
 }
 
 /// Delete a Gazelle project.
 ///
-/// Throws a [DeletingProjectError] if project does not exist.
+/// Throws a [DeleteProjectError] if project does not exist.
 
 Future<String> deleteProject({String? path}) async {
   final pathOption = path ?? Directory.current.path;
   if (!await Directory(pathOption).exists()) {
-    throw DeletingProjectError();
+    throw DeleteProjectError();
   }
   await Directory(pathOption).delete(recursive: true);
   return pathOption;
