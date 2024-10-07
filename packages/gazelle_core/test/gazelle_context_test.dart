92f6052c275875be9490fec94ba53527a7f3ed7d
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2024-10-07 11:41:12 +0200

    test(gazelle_core): Headers tests
    Added missing tests for `GazelleHttpHeaderExtension`

--- packages/gazelle_core/test/gazelle_context_test.dart
+++ packages/gazelle_core/test/gazelle_context_test.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:gazelle_core/gazelle_core.dart';
+import 'package:http/http.dart' as http;
 import 'package:test/test.dart';
 
 class _TestPlugin extends GazellePlugin {
@@ -17,6 +20,11 @@ class _TestPlugin2 extends GazellePlugin {
   Future<void> initialize(GazelleContext context) async {}
 
   bool get foo => true;
+}
+
+class _TestModelProvider extends GazelleModelProvider {
+  @override
+  Map<Type, GazelleModelType> get modelTypes => {};
 }
 
 void main() {
@@ -46,8 +54,8 @@ void main() {
       );
 
       // Act
-      await parentContext.register(_TestPlugin());
-      await childContext.register(_TestPlugin2());
+      await parentContext.register(const _TestPlugin());
+      await childContext.register(const _TestPlugin2());
 
       // Assert
       expect(childContext.getPlugin<_TestPlugin2>().foo, isTrue);
@@ -57,6 +65,57 @@ void main() {
         parentContext.getPlugin<_TestPlugin2>();
         fail('Should not be able to find plugin.');
       } catch (_) {}
+    });
+
+    test('Should create a new context', () {
+      // Arrange
+      final modelProvider = _TestModelProvider();
+
+      // Act
+      final context = GazelleContext.create(modelProvider: modelProvider);
+
+      // Assert
+      expect(context.modelProvider, isNotNull);
+    });
+
+    test('Should add and find routes', () async {
+      // Arrange
+      final routes = [GazelleRoute(name: 'a')];
+      final context = GazelleContext.create();
+      final server = await HttpServer.bind(InternetAddress.anyIPv4, 0);
+
+      GazelleRouterSearchResult? result;
+      server.listen((request) {
+        result = context.searchRoute(request);
+        request.response.close();
+      });
+
+      // Act
+      context.addRoutes(routes);
+      await http
+          .get(Uri.parse("http://${server.address.address}:${server.port}/a"));
+
+      // Assert
+      expect(result?.route.name, "a");
+
+      // Teardown
+      await server.close(force: true);
+    });
+
+    test('Should register a list of plugins', () async {
+      // Arrange
+      const plugins = {
+        _TestPlugin(),
+        _TestPlugin2(),
+      };
+      final context = GazelleContext.create();
+
+      // Act
+      await context.registerPlugins(plugins);
+
+      // Assert
+      expect(context.getPlugin<_TestPlugin>(), plugins.first);
+      expect(context.getPlugin<_TestPlugin2>(), plugins.last);
     });
   });
 }
--- packages/gazelle_core/test/gazelle_message_test.dart
+++ packages/gazelle_core/test/gazelle_message_test.dart
@@ -31,7 +31,133 @@ void main() {
         expect(request?.uri.path, uri.path);
         expect(request?.headers.isNotEmpty, isTrue);
         expect(request?.method, GazelleHttpMethod.post);
-        //       expect(await request?.body, "test");
+      });
+    });
+
+    group('GazelleMessageHeaderExtension tests', () {
+      final headerGetters =
+          <String, GazelleHttpHeader? Function(List<GazelleHttpHeader>)>{
+        'accept': (headers) => headers.accept,
+        'acceptcharset': (headers) => headers.acceptCharset,
+        'acceptencoding': (headers) => headers.acceptEncoding,
+        'acceptlanguage': (headers) => headers.acceptLanguage,
+        'acceptranges': (headers) => headers.acceptRanges,
+        'accesscontrolallowcredentials': (headers) =>
+            headers.accessControlAllowCredentials,
+        'accesscontrolallowheaders': (headers) =>
+            headers.accessControlAllowHeaders,
+        'accesscontrolallowmethods': (headers) =>
+            headers.accessControlAllowMethods,
+        'accesscontrolalloworigin': (headers) =>
+            headers.accessControlAllowOrigin,
+        'accesscontrolexposeheaders': (headers) =>
+            headers.accessControlExposeHeaders,
+        'accesscontrolmaxage': (headers) => headers.accessControlMaxAge,
+        'accesscontrolrequestheaders': (headers) =>
+            headers.accessControlRequestHeaders,
+        'accesscontrolrequestmethod': (headers) =>
+            headers.accessControlRequestMethod,
+        'age': (headers) => headers.age,
+        'allow': (headers) => headers.allow,
+        'authorization': (headers) => headers.authorization,
+        'cachecontrol': (headers) => headers.cacheControl,
+        'connection': (headers) => headers.connection,
+        'contentdisposition': (headers) => headers.contentDisposition,
+        'contentencoding': (headers) => headers.contentEncoding,
+        'contentlanguage': (headers) => headers.contentLanguage,
+        'contentlength': (headers) => headers.contentLength,
+        'contentlocation': (headers) => headers.contentLocation,
+        'contentrange': (headers) => headers.contentRange,
+        'contenttype': (headers) => headers.contentType,
+        'cookie': (headers) => headers.cookie,
+        'date': (headers) => headers.date,
+        'etag': (headers) => headers.etag,
+        'expect': (headers) => headers.expect,
+        'expires': (headers) => headers.expires,
+        'forwarded': (headers) => headers.forwarded,
+        'from': (headers) => headers.from,
+        'host': (headers) => headers.host,
+        'ifmatch': (headers) => headers.ifMatch,
+        'ifmodifiedsince': (headers) => headers.ifModifiedSince,
+        'ifnonematch': (headers) => headers.ifNoneMatch,
+        'ifrange': (headers) => headers.ifRange,
+        'ifunmodifiedsince': (headers) => headers.ifUnmodifiedSince,
+        'lastmodified': (headers) => headers.lastModified,
+        'link': (headers) => headers.link,
+        'location': (headers) => headers.location,
+        'maxforwards': (headers) => headers.maxForwards,
+        'origin': (headers) => headers.origin,
+        'pragma': (headers) => headers.pragma,
+        'proxyauthenticate': (headers) => headers.proxyAuthenticate,
+        'proxyauthorization': (headers) => headers.proxyAuthorization,
+        'range': (headers) => headers.range,
+        'referer': (headers) => headers.referer,
+        'retryafter': (headers) => headers.retryAfter,
+        'secwebsocketaccept': (headers) => headers.secWebSocketAccept,
+        'secwebsocketextensions': (headers) => headers.secWebSocketExtensions,
+        'secwebsocketkey': (headers) => headers.secWebSocketKey,
+        'secwebsocketprotocol': (headers) => headers.secWebSocketProtocol,
+        'secwebsocketversion': (headers) => headers.secWebSocketVersion,
+        'server': (headers) => headers.server,
+        'setcookie': (headers) => headers.setCookie,
+        'stricttransportsecurity': (headers) => headers.strictTransportSecurity,
+        'te': (headers) => headers.te,
+        'trailer': (headers) => headers.trailer,
+        'transferencoding': (headers) => headers.transferEncoding,
+        'upgrade': (headers) => headers.upgrade,
+        'useragent': (headers) => headers.userAgent,
+        'vary': (headers) => headers.vary,
+        'via': (headers) => headers.via,
+        'wwwauthenticate': (headers) => headers.wwwAuthenticate,
+      };
+
+      test('All header getters should work correctly ', () {
+        final headers = GazelleHttpHeader.predefinedValues
+            .map((header) => GazelleHttpHeader.fromString(header.header,
+                values: ['test-value']))
+            .toList();
+
+        for (final predefinedHeader in GazelleHttpHeader.predefinedValues) {
+          final headerName =
+              predefinedHeader.header.toLowerCase().replaceAll('-', '');
+          final getter = headerGetters[headerName];
+
+          expect(
+            getter,
+            isNotNull,
+            reason: 'Getter for $headerName should exist',
+          );
+
+          final result = getter!(headers);
+          expect(
+            result,
+            isNotNull,
+            reason: 'Getter for $headerName should not return null',
+          );
+          expect(
+            result!.header.replaceAll("-", "").toLowerCase(),
+            headerName.toLowerCase(),
+            reason: 'Header name mismatch for $headerName',
+          );
+          expect(
+            result.values,
+            ['test-value'],
+            reason: 'Header value mismatch for $headerName',
+          );
+        }
+
+        for (final predefinedHeader in GazelleHttpHeader.predefinedValues) {
+          final headerName =
+              predefinedHeader.header.toLowerCase().replaceAll('-', '');
+          final getter = headerGetters[headerName];
+
+          final result = getter!(<GazelleHttpHeader>[]);
+          expect(
+            result,
+            isNull,
+            reason: 'Getter for $headerName should return null for empty list',
+          );
+        }
       });
     });
   });
